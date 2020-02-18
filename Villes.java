87b1c922d9c5f1e685abb5cbd338847156a441c8
ZachDes07 <zacou07@hotmail.com>
2020-02-18 16:17:58 -0500

    Mardi

--- Villes.java
+++ Villes.java
@@ -8,6 +8,32 @@ public class Villes {
 
         for(int i = 0; i < nbLignes; i++)
             for(int j = 0; j < nbColonnes; j++)
-                System.out.println(tab[i][j]);
+                System.out.println(tab[i][j] + "");
     }
+
+    public static int sommeUneLigne(int[][] tab, int iLigne){
+        int somme = 0;
+
+        for(int i = 0; i < tab[iLigne].length; i++) {
+            somme = somme + tab[iLigne][i];
+            iLigne++;
+        }
+
+        return somme;
+    }
+
+    public static int moyenneUneLigne(int [][] tab, int iLigne){
+        int nbColonnes = tab[0].length;
+        int moyenne = 0;
+        int somme = 0;
+
+        for(int i = 0; i < tab[iLigne].length; i++) {
+            somme = somme + tab[iLigne][i];
+        }
+
+        moyenne = somme / nbColonnes;
+
+        return moyenne;
+    }
+
 }
